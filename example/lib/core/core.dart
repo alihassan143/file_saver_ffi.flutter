1a6cdd034b3b73f0767dc2d5fc473574dee0d39a
vanvixi <vanvixi.dev@gmail.com>
2026-02-08 23:40:15 +0700

    refactor(example): remove MediaSaverStateMixin and clean up imports in core files

--- example/lib/core/core.dart
+++ example/lib/core/core.dart
@@ -3,7 +3,6 @@ library;
 
 export 'file_type_config.dart';
 export 'file_utils.dart';
-export 'media_saver_mixin.dart';
 export 'demo_save_mixin.dart';
 export 'save_demo_common.dart';
 export 'utils.dart';
