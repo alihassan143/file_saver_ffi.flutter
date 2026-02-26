6833f1ff31f0f7e86558111824547c5edfa81279
vanvixi <vanvixi.dev@gmail.com>
2026-02-26 10:34:43 +0700

    fix(example/windows): remove unused plugin registrations for file_saver_ffi

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,12 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <file_saver_ffi/none.h>
 #include <file_selector_windows/file_selector_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  noneRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("none"));
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
 }
