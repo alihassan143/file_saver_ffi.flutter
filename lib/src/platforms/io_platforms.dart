924042ad0fc41f215a1898834592652c6bea0394
vanvixi <vanvixi.dev@gmail.com>
2026-03-03 15:43:15 +0700

    feat(platform): add stubs for non-web platform implementations

---
+++ lib/src/platforms/io_platforms.dart
@@ -0,0 +1,6 @@
+// All non-web (IO) platform implementations grouped into one file.
+// This file is only compiled on non-web platforms (dart.library.io).
+export 'android/file_saver_android.dart';
+export 'darwin/file_saver_darwin.dart';
+export 'linux/file_saver_linux.dart';
+export 'windows/file_saver_windows.dart';
---
+++ lib/src/platforms/io_stub.dart
@@ -0,0 +1,83 @@
+// Stubs for all non-web platform classes, used when compiling for web.
+import 'dart:typed_data';
+
+import '../models/conflict_resolution.dart';
+import '../models/file_type.dart';
+import '../models/save_input.dart';
+import '../models/locations/save_location.dart';
+import '../models/save_progress.dart';
+import '../platform_interface/file_saver_platform.dart';
+
+class FileSaverAndroid extends _FileSaverStub {
+  FileSaverAndroid() : super('FileSaverAndroid');
+
+  static void registerWith() => FileSaverPlatform.instance = FileSaverAndroid();
+}
+
+class FileSaverDarwin extends _FileSaverStub {
+  FileSaverDarwin() : super('FileSaverDarwin');
+
+  static void registerWith() => FileSaverPlatform.instance = FileSaverDarwin();
+}
+
+class FileSaverLinux extends _FileSaverStub {
+  FileSaverLinux() : super('FileSaverLinux');
+
+  static void registerWith() => FileSaverPlatform.instance = FileSaverLinux();
+}
+
+class FileSaverWindows extends _FileSaverStub {
+  FileSaverWindows() : super('FileSaverWindows');
+
+  static void registerWith() => FileSaverPlatform.instance = FileSaverWindows();
+}
+
+class _FileSaverStub extends FileSaverPlatform {
+  _FileSaverStub(this.className);
+
+  final String className;
+
+  @override
+  void dispose() {}
+
+  @override
+  Stream<SaveProgress> saveBytes({
+    required Uint8List fileBytes,
+    required FileType fileType,
+    required String fileName,
+    SaveLocation? saveLocation,
+    String? subDir,
+    ConflictResolution conflictResolution = ConflictResolution.autoRename,
+  }) => throw UnsupportedError('$className is not supported on this platform.');
+
+  @override
+  Stream<SaveProgress> saveFile({
+    required String filePath,
+    required String fileName,
+    required FileType fileType,
+    SaveLocation? saveLocation,
+    String? subDir,
+    ConflictResolution conflictResolution = ConflictResolution.autoRename,
+  }) => throw UnsupportedError('$className is not supported on this platform.');
+
+  @override
+  Stream<SaveProgress> saveNetwork({
+    required String url,
+    required String fileName,
+    required FileType fileType,
+    Map<String, String>? headers,
+    Duration timeout = const Duration(seconds: 60),
+    SaveLocation? saveLocation,
+    String? subDir,
+    ConflictResolution conflictResolution = ConflictResolution.autoRename,
+  }) => throw UnsupportedError('$className is not supported on this platform.');
+
+  @override
+  Stream<SaveProgress> saveAs({
+    required SaveInput input,
+    required FileType fileType,
+    required String fileName,
+    required UserSelectedLocation saveLocation,
+    ConflictResolution conflictResolution = ConflictResolution.autoRename,
+  }) => throw UnsupportedError('$className is not supported on this platform.');
+}
